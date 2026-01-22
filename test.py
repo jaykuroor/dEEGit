b60f56ba789619f6a65e41bf4a18534a19fad337
jay kuroor <jaykuroor@outlook.com>
2026-01-22 08:36:33 -0600

    add temporary code to validate parsing

--- test.py
+++ test.py
@@ -2,9 +2,7 @@ from src.parse import parse_events, events_to_tensor
 
 events = parse_events("data/IN.txt", min_len=250, target_len=256)
 
-# Put your true 5 channels here in the order you want.
-# If you don't care about order, sorted(...) is fine as long as it's consistent.
-channels = ["AF3", "AF4", "T7", "T8", "PZ"]  # <- replace with your actual 5
+channels = ["AF3", "AF4", "T7", "T8", "PZ"]
 
 X, y = events_to_tensor(events, channels)
 print("events stored:", len(events))
